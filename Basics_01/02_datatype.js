8a1b47346e91b08ac22e216838d0ddd96b9fcb99
dev0405 <gargdk2005@gmail.com>
2026-01-08 22:54:02 +0530

    added modification in datatype

--- Basics_01/02_datatype.js
+++ Basics_01/02_datatype.js
@@ -1,11 +1,14 @@
-console.log(typeof 123);
+// console.log(typeof 123);
 
-console.log(typeof "hello");
+// console.log(typeof "hello");
 
-console.log(typeof true)
+// console.log(typeof true)
 
-console.log(typeof null);
+// console.log(typeof null);
 
-console.log(typeof undefined);
+// console.log(typeof undefined);
 
+// const id=Symbol("123");
+// const id1=Symbol("123");
 
+// console.log(id===id1)
