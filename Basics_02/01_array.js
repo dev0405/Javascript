bf22aabfddac089e231768b2f3a849c783c60c10
dev0405 <gargdk2005@gmail.com>
2026-01-09 23:22:33 +0530

    added file for array

---
+++ Basics_02/01_array.js
@@ -0,0 +1,48 @@
+const arr=[1,2,3,4,5]
+// console.log(arr)
+
+// arr.push(6)
+// console.log(arr);
+
+// arr[0]=9
+// console.log(arr);
+
+arr.pop()
+// console.log(arr);
+
+arr.unshift(9)
+// console.log(arr);
+
+arr.shift()
+// console.log(arr);
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(1));
+
+//**************************************************** 
+// console.log(arr.slice(1,3))
+// console.log(arr);
+
+// console.log(arr.splice(1,3))
+// console.log(arr);
+
+
+const arr1=[5,6,7,8]
+// console.log(arr.concat(arr1))
+// console.log(arr);
+// console.log(arr1);
+
+// const arr2=[...arr,...arr1]
+// console.log(arr2);
+// console.log(arr);
+// console.log(arr1);
+
+// const arr3=[1,2,3,4,[1,23,4,3]]
+// console.log(arr3.flat(2))
+// console.log(arr3);
+
+// console.log(Array.isArray(arr1));
+
+// console.log(Array.from('hello'));
+
+console.log(Array.of(4,5,2,3,4));
