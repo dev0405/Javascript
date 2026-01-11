51a16a49ee3330210a4a58c7c0c07ad4720b2d90
dev0405 <gargdk2005@gmail.com>
2026-01-11 23:31:09 +0530

    added destructuring concept in object file

--- Basics_02/02_objects.js
+++ Basics_02/02_objects.js
@@ -46,10 +46,19 @@ const obj3={...user,...jsUser} //spread method
 // console.log(obj3);
 
 //array of keys
-console.log(Object.keys(user));
+// console.log(Object.keys(user));
 
 //array of values
-console.log(Object.values(user));
+// console.log(Object.values(user));
 
 //arrauy of enteries
-console.log(Object.entries(user));
+// console.log(Object.entries(user));
+
+//destructuring of objects
+const course={
+    naame:"JS",
+    duration:10,
+    price:'999'
+}
+const {duration}=course
+console.log(duration)
