4b0dfbaab8442beac48d8ff96fe09ec1c6203454
dev0405 <gargdk2005@gmail.com>
2026-01-18 23:09:40 +0530

    added if else concept in control flow folder

---
+++ ControlFlow_03/ifelse.js
@@ -0,0 +1,18 @@
+const a = 40;
+// if(a>30){
+//     console.log("yes");
+    
+// }else{
+//     console.log("no");
+    
+// }
+if(a>30){
+    console.log("A");
+    
+}else if(a>20){
+    console.log("B");
+    
+}else{
+    console.log("C");
+    
+}
