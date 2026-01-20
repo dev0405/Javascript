a194a0f37056a7427b65ea0669198e50000d3ac2
dev0405 <gargdk2005@gmail.com>
2026-01-20 22:50:16 +0530

    added exrra concept in control flow folder

---
+++ ControlFlow_03/extra.js
@@ -0,0 +1,24 @@
+// falsy values
+// false,0,-0,BigInt,"",null,undefined,Nan
+
+//truthly values
+//true,1, "0","false"," ",[],{},function(){}
+
+// console.log(""==false)
+// console.log(1==true)
+
+//Nullish coalescing operator ?? null undefined
+
+// let val1;
+// val1=5??10
+// console.log(val1);
+// val1=null??10
+// console.log(val1);
+// val1=undefined??10
+// console.log(val1);
+
+//Terniary operator  condition?true:false
+
+const val=10
+val>=5?console.log("yes"):console.log("no");
+
