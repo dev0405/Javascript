06472441e99d67d102ffe183f7ebebb60e742bb3
dev0405 <gargdk2005@gmail.com>
2026-01-21 22:33:38 +0530

    added for while do-while concept

---
+++ Iteration_04/forandwhile.js
@@ -0,0 +1,20 @@
+const arr=[1,2,3,4,5]
+
+// for loop
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+//while loop
+// let i=0;
+// while(i<5){
+//     console.log(i);
+//     i++
+// }
+
+//do-while loop
+let score=1
+do{
+    console.log(score);
+    score++
+}while(score<5)
