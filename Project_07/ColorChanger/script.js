87270c5d142e6357921f65dea54b137b7812e451
dev0405 <gargdk2005@gmail.com>
2026-01-25 22:18:54 +0530

    added script file in color change project

---
+++ Project_07/ColorChanger/script.js
@@ -0,0 +1,15 @@
+console.log("Hello")
+const buttons=document.querySelectorAll(".button")
+console.log(buttons);
+const body=document.querySelector("body")
+console.log(body);
+
+buttons.forEach((button)=>{
+    // console.log(button)
+    button.addEventListener("click",(e)=>{
+        // console.log(e);
+        console.log(e.target);
+        body.style.backgroundColor=e.target.id
+        
+    })
+})
