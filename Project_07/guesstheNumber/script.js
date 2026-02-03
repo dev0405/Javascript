7ccce2c39f588757490bc8bb2c17d5ee7b3060bd
dev0405 <gargdk2005@gmail.com>
2026-02-03 11:10:00 +0530

    Updated script file

--- Project_07/guesstheNumber/script.js
+++ Project_07/guesstheNumber/script.js
@@ -5,7 +5,7 @@ const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
 const guessSlot = document.querySelector('.guesses');
 const remaining = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
+const lowOrHi = document.querySelector('.lowOrHigh');
 const startOver = document.querySelector('.resultParas');
 
 const p = document.createElement('p');
